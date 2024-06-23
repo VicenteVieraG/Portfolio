196259161ea3addec7f41410977517f7869de543
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-06-22 18:28:51 -0600

    Styled grid container. - Added common class for cards.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				"dark-blue": "#1B263B",
+				"dark-gray": "#2C2C2C"
+			}
+		}
 	},
 	plugins: [require('@tailwindcss/typography')]
 };
