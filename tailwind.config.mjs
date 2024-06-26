3292cd93b1ae5bd41edf30ebce474bf6c6a0f04d
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-06-26 01:34:12 -0600

    Source formated

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			colors: {
-				"Spurple": "#3F0071",
-				"Spink": "#FB2576",
-				"Sblue": "#332FD0",
-				"Snavy": "#0002A1",
-				"select-color": "var(--select-color)"
-			}
-		}
-	},
-	plugins: [require('@tailwindcss/typography')]
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                Spurple: "#3F0071",
+                Spink: "#FB2576",
+                Sblue: "#332FD0",
+                Snavy: "#0002A1",
+                "select-color": "var(--select-color)"
+            }
+        }
+    },
+    plugins: [require("@tailwindcss/typography")]
 };
